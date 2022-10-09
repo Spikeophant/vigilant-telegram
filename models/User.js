cb66e4103f47b1079bf0c9d03bed213477ee6b43
Dustin Davisson <dustin.davisson@gmail.com>
2022-10-09 14:14:25 -0700

    Add bcrypt, update Models syntax, public dir, seeds.

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const { Model, DataTypes, DataTypes} = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
@@ -53,8 +53,6 @@ User.init(
         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
       },
     },
-  },
-  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const session = require('express-session');
 
 const db = require('./config/connection');
+const path = require('path');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -21,6 +22,7 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 db.sync({ force: false }).then(() => {
   app.listen(PORT,() => console.log('Now listening'));
